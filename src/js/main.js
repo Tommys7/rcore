25b96759a46f5c91431f3ce3aa937a865a7fea53
tommys7 <tomas.hojgr23@gmail.com>
2023-02-21 15:40:02 +0100

    kodovani sekce main, header, releases + napojeni slideru + respozivita

--- src/js/main.js
+++ src/js/main.js
@@ -22,4 +22,17 @@ $(document).ready(function(){
             }
     })
 
+
+
+    $('.main-slider').slick({
+      
+        infinite: true,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        arrows: true,
+        prevArrow: $('.main-slider__prevArrow'),
+        nextArrow: $('.main-slider__nextArrow')
+      });
+
+
 });
